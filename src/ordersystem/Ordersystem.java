0a798fd48df6bb3221f6eb960e735bdd7a94aee4
nss5161 <nss5161@202-nwd46.up.ist.local>
2016-01-26 18:56:31 -0500

    Merge origin/master
    Conflicts:
    	Helicopter/src/helicopter/Game.java

--- src/ordersystem/Ordersystem.java
+++ src/ordersystem/Ordersystem.java
@@ -1,3 +1,5 @@
+package ordersystem;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,9 +10,6 @@ import java.util.*;
 import static javax.swing.UIManager.put;
 import ordersystem.InventoryItem;
 import ordersystem.Transaction;
-
-
-package ordersystem;
 
 /**
  *
