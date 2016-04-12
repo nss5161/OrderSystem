663aa01756594dce8d92c03e1369e17f666526c8
Nick <Nick@client-104-39-89-208.mobility.psu.edu>
2016-04-12 18:39:57 -0400

    inir

--- src/ordersystem/Sale.java
+++ src/ordersystem/Sale.java
@@ -10,4 +10,58 @@ package ordersystem;
  * @author nss5161
  */
 public class Sale {
+    private int orderID;
+    private double total;
+    private int productID;
+    private int quantity;
+    private int type;
+    
+    public Sale(int newProductID, int newQuantity, Customer newCustomer) {
+        this.processTransaction(newProductID, newQuantity, newCustomer);
+    }
+    
+    public synchronized final boolean processTransaction(int theProductID, int theQuantity, Customer theCustomer) {
+        if(InventoryCollection.getInventory().getItemByID(theProductID).getQuantity() >= theQuantity) {
+            // Valid transaction; deduct quantity and process order
+            InventoryCollection.getInventory().getItemByID(theProductID).setQuantity(InventoryCollection.getInventory().getItemByID(theProductID).getQuantity() - theQuantity);
+            int newOrderID = Transaction.getNextOrderID();
+            this.orderID = newOrderID;
+            this.total = (InventoryCollection.getInventory().getItemByID(theProductID).getItemPrice()) * (theQuantity);
+            this.productID = theProductID;
+            this.quantity = theQuantity;
+            this.type = 0; // 0 = sale
+            
+            //CustomerCollection.getCustomers().getCustomerByID(theCustomer.getCustomerID()).addTransaction(this);
+            System.out.println("Valid transaction. Successful sale of " + theQuantity + " " + InventoryCollection.getInventory().getItemByID(theProductID).getDescription() + "(s) to " + theCustomer.getFullName() + " for a total of $" + this.total + "." + " (Order ID: " + this.orderID + ")");
+            return true;
+        }else{
+            // Offer to allow sale of entire current stock instead
+            System.out.println("The quantity of " + InventoryCollection.getInventory().getItemByID(theProductID).getDescription() + " you requested is not available. Would you like to purchase the remaining stock (" + InventoryCollection.getInventory().getItemByID(theProductID).getQuantity() + ")? [y/n]");
+            return false;   
+        }
+    }
+
+    public int getOrderID() {
+        return this.orderID;
+    }
+    
+    public double getTotal() {
+        return this.total;
+    }
+    
+    public int getProductID() {
+        return this.productID;
+    }
+    
+    public int getQuantity() {
+        return this.quantity;
+    }
+    
+    public int getType() {
+        return this.type;
+    }
+
+    public void printTransactionDetails() {
+        System.out.println(this.orderID + " (sale)\t\t" + this.productID + " (" + InventoryCollection.getInventory().getItemByID(this.productID).getDescription() + ")\t\t" + this.quantity + "\t\t$" + this.total);
+    }
 }
--- src/ordersystem/Transaction.java
+++ src/ordersystem/Transaction.java
@@ -11,24 +11,28 @@ package ordersystem;
  */
 public abstract class Transaction{
     
-    private final int transactionID;
+    private static int nextOrderID;
     
-    public Transaction(int newID){
-        this.transactionID = newID;
-    }
+    public abstract boolean processTransaction(int theProductID, int theQuantity, Customer theCustomer);
     
-    public void setTransactionID(int newID){
+    public static int getNextOrderID() {
+        if (nextOrderID > 0) {
+            nextOrderID++;
+        } else {
+            nextOrderID = 1;
+        }
+        return nextOrderID;
     }
     
     public abstract int getOrderID();
-    
-    public abstract int getTotal();
-    
+
+    public abstract double getTotal();
+
     public abstract int getProductID();
-    
+ 
     public abstract int getQuantity();
-    
+
     public abstract int getType();
-    
-    public abstract int printTransactionDetails();
+
+    public abstract void printTransactionDetails();
 }
